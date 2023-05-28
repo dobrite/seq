063d88a83509888fa87c4860998a156141a8e69c
Dave O <davidobrite@gmail.com>
2023-05-27 17:17:12 -0700

    chore(math): add ceil

--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,17 @@
 #[inline(always)]
+pub(crate) fn ceil(val: f32) -> f32 {
+    let floor = val as u32 as f32;
+
+    if val == floor {
+        val
+    } else if val > 0.0 {
+        floor + 1.0
+    } else {
+        floor
+    }
+}
+
+#[inline(always)]
 pub(crate) fn round(val: f32) -> f32 {
     let floor = val as u32 as f32;
 
