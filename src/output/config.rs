94adf6c531d1fc420e8e6741dabadfd74012b908
Dave O <davidobrite@gmail.com>
2023-05-29 11:44:07 -0700

    chore(config): attribute order

--- src/output/config.rs
+++ src/output/config.rs
@@ -4,10 +4,10 @@ use super::{Density, Length, OutputType, Prob, Pwm, Rate};
 pub struct Config {
     pub density: Density,
     pub length: Length,
+    pub output_type: OutputType,
     pub prob: Prob,
     pub pwm: Pwm,
     pub rate: Rate,
-    pub output_type: OutputType,
 }
 
 impl Default for Config {
@@ -21,10 +21,10 @@ impl Config {
         Self {
             density: Density(4),
             length: Length(16),
+            output_type: OutputType::Gate,
             prob: Prob::P100,
             pwm: Pwm::P50,
             rate: Rate::Unity,
-            output_type: OutputType::Gate,
         }
     }
 }
